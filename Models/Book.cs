d65112dbba954af8cf076cab402e86e14d1ae0f5
hasan <hasan@email.com>
2026-08-07 14:14:16 +0300

    Initial project setup with book entity and database context

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace LibraryApi.Models;
+
+public class Book
+{
+    public int Id { get; set;}
+    public string Name { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(optins=>
+optins.UseSqlite("Data Source=LibraryApi.db"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
